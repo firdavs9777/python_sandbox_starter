531c8d87aceca6ea424e3ad553f7463704242773
firdavs9777 <fmutalipov97@gmail.com>
2024-11-12 08:39:37 +0900

    Loops Added

--- loops.py
+++ loops.py
@@ -1,5 +1,22 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
+fruits = ['apple', 'banana','cherry']
+for fruit in fruits:
+  print(fruit)
 
+# For Loop
+for i in range(5):
+  print(i)
 
 # While loops execute a set of statements as long as a condition is true.
+count = 0
+while count < 5:
+  print(count)
+  count += 1
+
+print('BreaK')
+# Break Example 
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
